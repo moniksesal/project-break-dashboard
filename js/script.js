7486ee10de297dc857498cbe2f7be8fc5a318d7d
Monica Serrano <monicasesal@gmail.com>
2025-12-09 16:37:01 +0100

    Random background uploaded

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const photos = [
+    "./assets/img/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
+    "./assets/img/billy-huynh-v9bnfMCyKbg-unsplash.jpg",
+    "./assets/img/ant-rozetsky-H9m6mfeeakU-unsplash.jpg",
+    "./assets/img/chad-madden-cPa-7yByq3o-unsplash.jpg",
+    "./assets/img/federica-galli-pF1ug8ysTtY-unsplash.jpg",
+    "./assets/img/andrey-grinkevich-0x6RTts1jRU-unsplash.jpg",
+    "./assets/img/chuttersnap-TSgwbumanuE-unsplash.jpg",
+    "./assets/img/vivek-doshi-obsBswnv7FI-unsplash.jpg",
+    "./assets/img/michal-mancewicz-R9L7ukhBSgs-unsplash.jpg",
+    "./assets/img/john-towner-FD_sabE544U-unsplash.jpg",
+    "./assets/img/torsten-dederichs-KrQJzrZiCak-unsplash.jpg",
+    "./assets/img/c-g-JgDUVGAXsso-unsplash.jpg",
+    "./assets/img/alex-dukhanov-ZxZQk7777R4-unsplash.jpg",
+    "./assets/img/shot-by-cerqueira-0o_GEzyargo-unsplash.jpg",
+    "./assets/img/jeremy-thomas-E0AHdsENmDg-unsplash.jpg"
+]
+
+function changeBackground() {
+    const random = Math.floor(Math.random() * photos.length)
+    document.body.style.backgroundImage = `url(${photos[random]})`
+}
+
+changeBackground()
+
+setInterval(changeBackground, 15000)
