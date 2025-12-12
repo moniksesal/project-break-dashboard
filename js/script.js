d19ef88222d8bb0c105e7c0ddb77e007415674fc
Monica Serrano <monicasesal@gmail.com>
2025-12-12 19:00:55 +0100

    fix problems and more responsive. adding images according to the color palette

--- js/script.js
+++ js/script.js
@@ -2,23 +2,25 @@ const photos = [
     "./assets/img/jason-mavrommatis-ap3LXI0fPJY-unsplash.jpg",
     "./assets/img/billy-huynh-v9bnfMCyKbg-unsplash.jpg",
     "./assets/img/ant-rozetsky-H9m6mfeeakU-unsplash.jpg",
-    "./assets/img/chad-madden-cPa-7yByq3o-unsplash.jpg",
-    "./assets/img/federica-galli-pF1ug8ysTtY-unsplash.jpg",
     "./assets/img/andrey-grinkevich-0x6RTts1jRU-unsplash.jpg",
     "./assets/img/chuttersnap-TSgwbumanuE-unsplash.jpg",
     "./assets/img/vivek-doshi-obsBswnv7FI-unsplash.jpg",
     "./assets/img/michal-mancewicz-R9L7ukhBSgs-unsplash.jpg",
     "./assets/img/john-towner-FD_sabE544U-unsplash.jpg",
     "./assets/img/torsten-dederichs-KrQJzrZiCak-unsplash.jpg",
-    "./assets/img/c-g-JgDUVGAXsso-unsplash.jpg",
-    "./assets/img/alex-dukhanov-ZxZQk7777R4-unsplash.jpg",
-    "./assets/img/shot-by-cerqueira-0o_GEzyargo-unsplash.jpg",
     "./assets/img/jeremy-thomas-E0AHdsENmDg-unsplash.jpg",
     "./assets/img/the-new-york-public-library-KCj0iqbuXtU-unsplash.jpg",
     "./assets/img/kien-do-NjT4O7WYmwk-unsplash.jpg",
     "./assets/img/meiying-ng-OrwkD-iWgqg-unsplash.jpg",
     "./assets/img/maria-noel-rabunal-cantero-i-8YnfyF0VY-unsplash.jpg",
-    "./assets/img/guillaume-galtier-3YrppYQPoCI-unsplash.jpg"
+    "./assets/img/guillaume-galtier-3YrppYQPoCI-unsplash.jpg",
+    "./assets/img/matt-hardy-55NI4yEAas4-unsplash.jpg",
+    "./assets/img/sarah-dorweiler-9Z1KRIfpBTM-unsplash.jpg",
+    "./assets/img/alex-b6C2oGuAads-unsplash.jpg",
+    "./assets/img/abhinav-chitikela-OtW3XS4Yjd0-unsplash.jpg",
+    "./assets/img/brad-mann-8U-vdPvBe50-unsplash.jpg",
+    "./assets/img/patrick-ryan-3kUIaB2EPp8-unsplash.jpg",
+    "./assets/img/erica-li-qbNcIFPC3qc-unsplash.jpg"
 ]
 
 function changeBackground() {
